c4f5c1de623598a367d7ff01aae1195b996c5332
Ming Cheng <102703663+hdkhdhdkh@users.noreply.github.com>
2022-04-12 21:35:00 -0400

    BondPrice_E change 1
    clear something

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -11,8 +11,3 @@ def getBondPrice_E(face, couponRate, m, yc):
  return(bondprice)   
 
 
-yc = [.010,.015,.020,.025,.030]
-face = 2000000
-couponRate = .04
-m = 5
-getBondPrice_E(face, couponRate, m, yc)
