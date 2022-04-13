0e6af0258cee99c1299f9d4e6499718b5d84adc2
Ming Cheng <102703663+hdkhdhdkh@users.noreply.github.com>
2022-04-12 21:47:16 -0400

    change 2
    change 2

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -3,7 +3,7 @@ def getBondPrice_E(face, couponRate, m, yc):
  pvcfsum =0
  cf = couponRate*face
  for i,y in enumerate(yc):
-    pv =(1+y)**(-(i+1))
+    pv = (1+y)**(-(i+1))
     pvcf = pv*cf
     pvcfsum += pvcf
     
